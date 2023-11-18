efe9b6251e588f6789c38631401c011da4ccb5c0
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-18 19:10:45 +0530

    Update ExerciseList1_Ans.py

--- ExerciseList1_Ans.py
+++ ExerciseList1_Ans.py
@@ -68,6 +68,15 @@ if (is_same(str1, str2)):
 else:
     print("Strings are not same")
 
+'''problem #5
+In your college Python is taught in 3 different departments by the same professor. 
+For each dept, get the no of students stutying Python and their marks in the final exam 
+Get the input as comma seperated string
+
+Find the top 3 marks in each class
+Find the top 3 marks if all classes are combined.
+Find the avg mark of students with passing mark in each class and the classes combined.
+Find which class has the best average mark and least number of failed students.'''
 
 
 
