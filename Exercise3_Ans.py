3037c29ec888f72cb2a84b8ac29e15c95162c85f
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-19 17:48:00 +0530

    Create Exercise3_Ans.py

---
+++ Exercise3_Ans.py
@@ -0,0 +1,13 @@
+'''1. Write a function to calculate compund interest. Help the user save money by displaying the interest they 
+can earn by saving certain % of thier salary, for 3 different interest rates and for 3 different % of salary
+eg
+  Input Rs50000
+  Output - If you save 10% of your salary, with 5% interest rate, you will get (display the amount)
+           If you save 10% of your salary, with 6.5% interest rate, you will get (display the amount)
+
+           If you save 15% of your salary, with 5% interest rate, you will get (display the amount)
+           If you save 15% of your salary, with 6.5% interest rate, you will get (display the amount)
+
+           If you save 15% of your salary, with 5% interest rate, you will get (display the amount)
+           If you save 15% of your salary, with 6.5% interest rate, you will get (display the amount)'''
+
