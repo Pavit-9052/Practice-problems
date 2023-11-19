39162bb2d2258cfdffaad3891fc46eb68e9a0ac6
Pavithra M <134091742+Pavit-9052@users.noreply.github.com>
2023-11-19 21:59:56 +0530

    Create SimpleGames.py

---
+++ SimpleGames.py
@@ -0,0 +1,33 @@
+'''
+Games
+
+Problem #1
+Play the game Rock Papar Scissors with the computer.
+Play it three times and best of three wins. 
+If you win, print your name in color (look for a python package to do this)
+Hint - Use random number generation
+
+Problem #2
+Two player dice game.
+Each player will roll the die (numbers from 1 to 6)
+Points are added to each roll.
+1 - 1 pt
+2 - 5 pts
+3 - 15 pts
+4 - (-15) pts
+5 - (-5) pts
+6 - (-1) pts
+
+Find which player scores 100 first and how many time they had to roll the die.
+Hint - use random numbers to generate the die (no need to get user input)
+Use Arrays and while loop.
+
+Problem #3
+Write an app for the follwoing two player game. You have a 6 x 6 board. 
+Users take turns rolling the dice twice. first roll is row no, second roll is col number. Mark the spot (row, col) as claimed by the user
+who rolled the dice.
+When the user rolls the dice within 1 col/row of a claimed spot of the other user, the other user gets a point. 
+If the spot is same as the claimed spot of the other user, the user that rolled the dice gets a point. 
+The player who gets 5 points first wins the game. 
+ 
+'''
